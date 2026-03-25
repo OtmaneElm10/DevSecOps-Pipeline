73c28ae06acae7a04063dc07b453404657477ece
EL MOUKI OTMANE <otmane.el-mouki@etu.univ-lyon1.fr>
2026-03-25 19:02:31 +0100

    fix user

--- backend/src/main/java/com/eventapp/model/entities/User.java
+++ backend/src/main/java/com/eventapp/model/entities/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * Represents a user in the application.
  */
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
