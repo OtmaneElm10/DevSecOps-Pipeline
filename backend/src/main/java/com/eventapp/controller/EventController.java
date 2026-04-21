afefea17bab78720e0824156f60a5d8e158b01b9
EL MOUKI OTMANE <otmane.el-mouki@etu.univ-lyon1.fr>
2026-04-21 17:11:58 +0200

    event endpoint

--- backend/src/main/java/com/eventapp/controller/EventController.java
+++ backend/src/main/java/com/eventapp/controller/EventController.java
@@ -2,14 +2,21 @@ package com.eventapp.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eventapp.model.entities.Event;
+import com.eventapp.model.entities.EventType;
 import com.eventapp.model.service.EventService;
 
 
@@ -32,19 +39,66 @@ public class EventController {
         this.eventService = eventService;
     }
 
-    @GetMapping
-    public List<Event> getAllEvents() {
-        return eventService.getAllEvents();
+    
+    @GetMapping(params = "!type")
+    public ResponseEntity<List<Event>> getAllEvents() {
+        return ResponseEntity.ok(eventService.getAllEvents());
     }
 
+    /**
+     * Get events by type.
+     * @param type event type
+     * @return list of events with the specified type
+     */
+    @GetMapping(params = "type")
+    public ResponseEntity<List<Event>> getEventsByType(@RequestParam final EventType type) {
+        return ResponseEntity.ok(eventService.getEventsByType(type));
     
+    }
+
+    /**
+     * Get event by ID.
+     * @param id event ID
+     * @return event with the specified ID
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable final Long id) {
+        return ResponseEntity.ok(eventService.getEventById(id));
+    }
+
+
     /**
      * Create new event.
      * @param event event
      */
     @PostMapping
-    public Event createEvent(final @RequestBody Event event) {
-        return eventService.createEvent(event);
+    public ResponseEntity<Event> createEvent(final @RequestBody Event event) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(eventService.createEvent(event));
     }
-}
 
+    
+    /**
+     * Update an existing event.
+     * @param id event ID to update
+     * @param event event data to update
+     * @return updated event
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable final Long id, 
+        final @RequestBody Event event) {
+        return ResponseEntity.ok(eventService.updateEvent(id, event));
+    }
+
+    
+    /**
+     * Delete an event by ID.
+     * @param id event ID to delete
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable final Long id) {
+        eventService.deleteEvent(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- backend/src/main/java/com/eventapp/repositories/EventRepository.java
+++ backend/src/main/java/com/eventapp/repositories/EventRepository.java
@@ -1,11 +1,22 @@
 package com.eventapp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eventapp.model.entities.Event;
+import com.eventapp.model.entities.EventType;
+
 
 /**
- * Repository for Event entity.
+ * Repository interface for Event entity, providing CRUD operations and custom query methods.
  */
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    /**
+     * Find events by their type.
+     * @param type type
+     * @return list of events with the specified type
+     */
+    List<Event> findByType(EventType type);
 }
