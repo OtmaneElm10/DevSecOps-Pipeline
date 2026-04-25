7a6cd6ef042549f2c4a6ee35a8b9f93b9e4c08a5
p2208755 <louis.duflo@gmail.com>
2026-04-25 12:41:08 +0200

    fix -<
    - Ajouter la dépendance H2 pour les tests unitaires
    - Configurer le plugin maven-surefire avec les arguments JVM nécessaires
    - Créer application.properties pour la configuration H2 en test
    - Modifier EventControllerTest pour utiliser @SpringBootTest au lieu de @WebMvcTest

--- backend/src/test/java/com/eventapp/controller/EventControllerTest.java
+++ backend/src/test/java/com/eventapp/controller/EventControllerTest.java
@@ -8,21 +8,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.time.LocalDate;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.eventapp.model.entities.Event;
 import com.eventapp.model.enums.EventType;
 import com.eventapp.model.service.EventService;
 
-@WebMvcTest(EventController.class)
+@SpringBootTest
 class EventControllerTest {
 
     @Autowired
+    private WebApplicationContext webApplicationContext;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @MockBean
     private EventService eventService;
