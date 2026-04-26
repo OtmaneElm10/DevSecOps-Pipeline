c162b2a4d496b139d2ac5458475feed3b5954841
p2208755 <louis.duflo@gmail.com>
2026-04-26 16:06:21 +0200

    centraliser la configuration CORS pour améliorer la sécurité

--- backend/src/main/java/com/eventapp/controller/AuthController.java
+++ backend/src/main/java/com/eventapp/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.eventapp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,6 @@ import com.eventapp.model.service.AuthService;
  */
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthService authService;
