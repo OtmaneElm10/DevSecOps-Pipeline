0a2ef4dca17792388a741d1ccb2d2fe8327a5330
p2208755 <louis.duflo@gmail.com>
2026-04-28 12:56:01 +0200

    Fix issue codage IP en dur

--- backend/src/main/java/com/eventapp/CorsConfig.java
+++ backend/src/main/java/com/eventapp/CorsConfig.java
@@ -1,17 +1,20 @@
 package com.eventapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "http://192.168.75.61")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
