d023c0d4ebca65c2e7888548d03e3b94089138bf
Otmane El Mouki <elmoukiotmane10@gmail.com>
2026-08-05 10:55:49 +0000

    site accessible depuis vps, bdd fonctionnelle aussi. il faut fix le role admin pour creer les events (ne marche pas). Prochaine etape configurer Sonarqube.

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // J'ai fais des tests en local donc verifier avec les autres si c'est la bonne adresse 
     // TODO: Mettre IP de la VM plus tard
-    baseURL: 'http://192.168.75.61/api', 
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
     }
